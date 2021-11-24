e16a49f66123d6f61a20320777b4d5d79c49e30c
xiaoqiu <isqiuya@qq.com>
2021-11-24 20:41:11 +0800

    three commit

--- CRUD/src/main/java/com/xiaoqiu/test/test.java
+++ CRUD/src/main/java/com/xiaoqiu/test/test.java
@@ -9,5 +9,6 @@ package com.xiaoqiu.test;
 public class test {
     public static void main(String[] args) {
         System.out.println("hello world!");
+        System.out.println("hello master!");
     }
 }
