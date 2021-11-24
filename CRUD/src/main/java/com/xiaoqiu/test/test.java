ba755b61b0546874f7d10e73698384cc61cecb92
xiaoqiu <isqiuya@qq.com>
2021-11-24 22:46:17 +0800

    four commit

--- CRUD/src/main/java/com/xiaoqiu/test/test.java
+++ CRUD/src/main/java/com/xiaoqiu/test/test.java
@@ -10,5 +10,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("hello world!");
         System.out.println("hello master!");
+        System.out.println("hello push!");
     }
 }
