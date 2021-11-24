250bc784c1821fc63bea760ee21091a98936879f
xiaoqiu <isqiuya@qq.com>
2021-11-24 20:24:31 +0800

    second commit

---
+++ CRUD/src/main/java/com/xiaoqiu/test/test.java
@@ -0,0 +1,13 @@
+package com.xiaoqiu.test;
+
+/**
+ * @author 小邱
+ * @version 0.0.1
+ * @description test
+ * @since 2021/11/24 20:23
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+}
